b695d6fb731d82a199a8ee57e47ab426ab3cd647
dlrandy <qq1208484996@qq.com>
2022-08-16 23:03:10 +0800

    feat: layout done

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import {
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={{ padding: 16, backgroundColor: "green", width: "100%" }}>
+        <Text>search</Text>
+      </View>
+      <View
+        style={{ flex: 1, padding: 16, backgroundColor: "blue", width: "100%" }}
+      >
+        <Text>list</Text>
+      </View>
+      <ExpoStatusBar style="auto" />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: "center",
+    backgroundColor: "orange",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // height: '100%', 测试出来和flex1差别不大
+    justifyContent: "center",
+    // 换成margintop？
+    paddingTop: StatusBar.currentHeight,
   },
 });
